007b19fdb8d03b894b7d0629a5c750d898e41820
sangwaniKondowe <bsc-com-06-17@cc.ac.mw>
2022-04-14 12:55:13 +0200

    jjj

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ const AuthRoutes = require('./Routes/loginRoute')
 const app = express();
 app.use(express.json())
 
+app.use(express.json())
+app.use(logger("common"));
+app.use(logger())
+
+
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+const bodyParser = require('body-parser');
+app.use(bodyParser.json())
+
 
 app.use('/login', AuthRoutes)
 

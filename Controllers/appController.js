a1e23b4c63e719f8f67709324883125b1abeee63
ASIMWELA <augastinesimwela@gmail.com>
2022-04-24 22:53:59 +0200

    op removed

--- Controllers/appController.js
+++ Controllers/appController.js
@@ -114,14 +114,14 @@ exports.markComplete = async (req, res) => {
     where: {
       gender: "female",
     },
-    order: ["gpa", "ASC"],
+    order: [["gpa", "ASC"]],
   });
 
   const malesArr = await Application.findAll({
     where: {
       gender: "males",
     },
-    order: ["gpa", "ASC"],
+    order: [["gpa", "ASC"]],
   });
 
   //holders for filtering
